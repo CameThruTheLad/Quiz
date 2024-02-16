4ad371311fb155e0d8e6f427f252afa30f776dee
Tomoya Sakai <zalaloom@gmail.com>
2024-02-16 21:53:55 +1300

    Updating the style of the log in page

---
+++ index.js
@@ -0,0 +1,13 @@
+var password = "hannyharman";
+
+function passCheck() {
+   
+    if(document.getElementById('password').value != password) {
+        alert('Wrong Password, try again pussy.');
+        return false;
+    }
+
+    if (document.getElementById('password').value == password) {
+
+    }
+}
